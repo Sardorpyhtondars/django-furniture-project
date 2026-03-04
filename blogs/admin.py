0a522a9d1692cab4ebb2db91220ca0ab3cc922d9
Sardorpythondars <sardoristamov1212@gmail.com>
2026-03-04 18:54:24 +0500

    translation added

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,28 +1,27 @@
 from django.contrib import admin
-
+from modeltranslation.admin import TabbedTranslationAdmin
 from blogs.models import Author, Category, Tag, Blog
 
-
 @admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
+class AuthorAdmin(TabbedTranslationAdmin):
     list_display = ['id', 'full_name', 'is_active', 'created_at']
     search_fields = ['full_name', 'professions']
-    list_filter = ['created_at', 'updated_at', 'is_active', 'professions']
+    list_filter = ['created_at', 'updated_at', 'is_active']
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TabbedTranslationAdmin):
     list_display = ['id', 'title', 'created_at']
-    search_fields = ['title', 'professions']
+    search_fields = ['title']
     list_filter = ['created_at', 'updated_at']
 
 @admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(TabbedTranslationAdmin):
     list_display = ['id', 'title', 'created_at']
-    search_fields = ['title', 'professions']
+    search_fields = ['title']
     list_filter = ['created_at', 'updated_at']
 
 @admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
+class BlogAdmin(TabbedTranslationAdmin):
     list_display = ['id', 'title', 'status', 'created_at']
     search_fields = ['title', 'short_description', 'long_description']
     list_filter = ['authors', 'categories', 'tags', 'status', 'created_at', 'updated_at']
