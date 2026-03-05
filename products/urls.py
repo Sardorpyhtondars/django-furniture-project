d5f793af4b26049da117e8ca6e383562412e5c9c
Sardorpythondars <sardoristamov1212@gmail.com>
2026-03-05 14:26:00 +0500

    translation locale added

--- products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from products.views import products_list_view
-
-app_name = 'products'
-
-urlpatterns = [
-    path('', products_list_view, name='list'),
-]
--- products/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def products_list_view(request):
-    return render(request, 'products/products-list.html')
